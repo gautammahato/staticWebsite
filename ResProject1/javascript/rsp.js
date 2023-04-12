1b09c9dd63b1589f9aa9225137628f8e55234931
Gautam Kumar Mahato <120447580+gautammahato@users.noreply.github.com>
2023-04-12 18:03:18 +0530

    Add files via upload

---
+++ ResProject1/javascript/rsp.js
@@ -0,0 +1,18 @@
+ burger = document.querySelector('.burger')
+ navbar = document.querySelector('.navbar')
+ rightNav = document.querySelector('.rightNav')
+ navList = document.querySelector('.nav-list')
+
+
+burger.addEventListener('click',()=>{
+
+    navbar.classList.toggle('v-class')  
+    navList.classList.toggle('v-class')  
+    navbar.classList.toggle('h-nav') 
+
+})
+
+
+
+
+
